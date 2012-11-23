f74b511eca6c620f74618a65ee3f35545f484b80
Charles <CMcFeaters@gmail.com>
2012-11-23 10:29:37 -0500

    edited main file for references

--- idea_box.py
+++ idea_box.py
@@ -16,4 +16,11 @@ search by headers
 	string: paragraph entry
 	string: search headers
 '''
-pass
+from idead_tables import User,Idea
+
+from sqlalchemy import create_engine,and_,or_
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import sys, string
+from operator import ne,eq,lt,le,ge,gt
+#make a new and improved search ability, make an importable class?
