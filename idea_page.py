5cd75bfd3dd730f766dbf290368b1b18dacfca94
Charles <CMcFeaters@gmail.com>
2013-02-02 11:30:14 -0500

    Created templates for addIdea and register, linked, need to create peruse template, start doing the python to db then create the search template

--- idea_page.py
+++ idea_page.py
@@ -17,14 +17,30 @@ def welcome():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	if request.method=='POST':
+		#check for valid log in
+		#if invalid: flash invalid message, redirect to 'welcome'
+		#if valid: flash welcome message, redirect to 'addIdea'
+		return redirect(url_for('addIdea'))
 	return render_template("welcome.html")
 
+@app.route('/addIdea',methods=['GET','POST'])
+def addIdea():
+	if request.method=='POST':
+		print request.form['body']
+		return render_template('addIdea.html')
+	else:
+		print request.method
+		return render_template('addIdea.html')
+	
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-	searchword=request.args.get('key','')
-	print searchword
+	
 	if request.method=='POST':
-		flash("I'M SURE THAT WORKED FINE")
+		#Need to check if we can create the user
+		#if create, write to db, flash message
+		#if not, flash why not
+		flash("Registration successful! Welcome to the fold %s"%request.form['regUsername'])
 		return redirect(url_for('welcome'))
 	return render_template("register.html")
 	
